9204eb4b8d5251e0eb563b388ab2417851c57132
jettro <jettro@gridshore.nl>
2012-07-01 16:34:51 +0200

    added the vertx component

---
+++ vertx-ui/trader/OrderHandler.groovy
@@ -0,0 +1,31 @@
+def eventBus = vertx.eventBus
+def companies = [:]
+
+eventBus.registerHandler("trader.trade.executed") { message ->
+    println "Order: ${message.body}"
+    def result = message.body
+    result.tradeExecuted.companyName = companies.get(result.tradeExecuted.orderbookId)
+    eventBus.send("updates.trades", message.body)
+}
+
+
+
+def mongoConfig = ["db_name": "axontrader"]
+container.with {
+    deployVerticle('mongo-persistor', mongoConfig, 1) {
+        println "Mongo is deployed"
+        def query = ["action": "find", "collection": "orderBookEntry", "matcher": [:]]
+        eventBus.send("vertx.mongopersistor", query) {message ->
+            message.body.results.each {orderbook ->
+                println "Add company for orderbook: ${orderbook._id} - ${orderbook.companyName}"
+                companies.put(orderbook._id, orderbook.companyName)
+            }
+        }
+    }
+}
+
+
+
+
+//vertx.setTimer(1000l) {
+//}
---
+++ vertx-ui/trader/TradeWatcher.groovy
@@ -0,0 +1,38 @@
+import groovy.json.JsonSlurper
+import org.vertx.groovy.core.http.RouteMatcher
+
+def server = vertx.createHttpServer()
+def routeMatcher = new RouteMatcher()
+def slurper = new JsonSlurper()
+def eb = vertx.eventBus
+
+routeMatcher.get("/") { req ->
+    req.response.sendFile("static/socketDemo.html")
+}
+
+routeMatcher.getWithRegEx("^\\/static\\/.*") { req ->
+    req.response.sendFile(req.path.substring(1))
+}
+
+routeMatcher.post("/executed") { request ->
+    println "received a trade executed event"
+
+    request.bodyHandler { body ->
+        def received = slurper.parseText(body.toString())
+        eb.send("trader.trade.executed", received)
+    }
+
+    request.response.putHeader("Content-Type", "application/json")
+    request.response.end("{\"status\":\"RECEIVED\"}")
+}
+
+server.requestHandler(routeMatcher.asClosure())
+
+vertx.createSockJSServer(server).bridge(prefix: '/eventbus', [[:]])
+
+server.listen(9090)
+
+container.deployVerticle("OrderHandler.groovy") {
+    println "OrderHandler verticle is deployed."
+}
+
