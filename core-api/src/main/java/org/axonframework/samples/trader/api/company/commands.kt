c43f4f2ca287289afe2375a996df809ad32734ff
smcvb <smcvbeelen@gmail.com>
2018-05-28 16:56:37 +0200

    Add missing @TargetAggregateIdentifier annotation
    Add missing @TargetAggregateIdentifier annotation

--- core-api/src/main/java/org/axonframework/samples/trader/api/company/commands.kt
+++ core-api/src/main/java/org/axonframework/samples/trader/api/company/commands.kt
@@ -1,7 +1,18 @@
 package org.axonframework.samples.trader.api.company
 
+import org.axonframework.commandhandling.TargetAggregateIdentifier
 import org.axonframework.samples.trader.api.orders.trades.OrderBookId
 import org.axonframework.samples.trader.api.users.UserId
 
-data class CreateCompanyCommand(val companyId: CompanyId, val userId: UserId, val companyName: String, val companyValue: Long, val amountOfShares: Long)
-data class AddOrderBookToCompanyCommand(val companyId: CompanyId, val orderBookId: OrderBookId)
+data class CreateCompanyCommand(
+        @TargetAggregateIdentifier val companyId: CompanyId,
+        val userId: UserId,
+        val companyName: String,
+        val companyValue: Long,
+        val amountOfShares: Long
+)
+
+data class AddOrderBookToCompanyCommand(
+        @TargetAggregateIdentifier val companyId: CompanyId,
+        val orderBookId: OrderBookId
+)
