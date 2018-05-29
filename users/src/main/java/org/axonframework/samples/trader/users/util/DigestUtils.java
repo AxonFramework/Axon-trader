b19b933597aa5147eacf79cc2c42daf81cf5b83d
smcvb <smcvbeelen@gmail.com>
2018-05-29 15:15:47 +0200

    Fixed some warnings
    Fixed some warnings
    
    #28

--- users/src/main/java/org/axonframework/samples/trader/users/util/DigestUtils.java
+++ users/src/main/java/org/axonframework/samples/trader/users/util/DigestUtils.java
@@ -16,22 +16,18 @@
 
 package org.axonframework.samples.trader.users.util;
 
-import org.springframework.util.Assert;
-
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
-/**
- * @author Allard Buijze
- */
-public class DigestUtils {
+import static org.springframework.util.Assert.notNull;
+import static org.springframework.util.Assert.state;
 
-    private static char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+public abstract class DigestUtils {
 
-    private DigestUtils() {
-        // prevent instantiation
-    }
+    private static final char[] HEX_CHARS =
+            {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+
 
     /**
      * Calculate the SHA1 hash for a given string.
@@ -40,18 +36,20 @@ public class DigestUtils {
      * @return the SHA1 Hash
      */
     public static String sha1(String text) {
-        Assert.notNull(text);
+        notNull(text);
         try {
             MessageDigest md = MessageDigest.getInstance("SHA1");
             return hex(md.digest(text.getBytes("UTF-8")));
         } catch (NoSuchAlgorithmException ex) {
             throw new IllegalStateException(
-                    "Unable to calculate hash. No SHA1 hasher available in this Java implementation",
-                    ex);
+                    "Unable to calculate hash. No SHA1 'hasher' available in this Java implementation",
+                    ex
+            );
         } catch (UnsupportedEncodingException ex) {
             throw new IllegalStateException(
                     "Unable to calculate hash. UTF-8 encoding is not available in this Java implementation",
-                    ex);
+                    ex
+            );
         }
     }
 
@@ -62,14 +60,15 @@ public class DigestUtils {
      * @return The hex representation of the hash value
      */
     public static String md5(String text) {
-        Assert.notNull(text);
+        notNull(text);
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
             return hex(md.digest(text.getBytes()));
         } catch (NoSuchAlgorithmException ex) {
             throw new IllegalStateException(
                     "Unable to calculate hash. No MD5 hasher available in this Java implementation",
-                    ex);
+                    ex
+            );
         }
     }
 
@@ -80,7 +79,7 @@ public class DigestUtils {
      * @return the hex representation of the String
      */
     public static String hex(byte[] data) {
-        Assert.notNull(data);
+        notNull(data);
         return hex(data, 0, data.length);
     }
 
@@ -93,12 +92,11 @@ public class DigestUtils {
      * @return the hex representation of the String
      */
     public static String hex(byte[] data, int offset, int length) {
-        Assert.notNull(data);
-        Assert.state(offset >= 0, "The offset must be positive");
-        Assert.state(offset < data.length, "The offset must be lower than the length of the data");
-        Assert.state(length >= 0, "The requested length must be positive");
-        Assert.state(length <= data.length,
-                     "The requested length must be equal to or lower than the length of the data");
+        notNull(data);
+        state(offset >= 0, "The offset must be positive");
+        state(offset < data.length, "The offset must be lower than the length of the data");
+        state(length >= 0, "The requested length must be positive");
+        state(length <= data.length, "The requested length must be equal to or lower than the length of the data");
 
         StringBuilder buf = new StringBuilder(length * 2);
         for (int i = offset; i < (offset + length); i++) {
@@ -109,5 +107,9 @@ public class DigestUtils {
             buf.append(HEX_CHARS[b & 0x0f]);
         }
         return buf.toString();
+    }
+
+    private DigestUtils() {
+        // Prevent instantiation as this is a utility class
     }
 }
