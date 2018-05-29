bd0a0a3fffd87c8bdc022566c940d33aad009e81
smcvb <smcvbeelen@gmail.com>
2018-05-29 13:24:30 +0200

    Add TransactionType as set field to start transaction commands
    Add TransactionType as set field to start transaction commands
    
    #28

--- core-api/src/main/java/org/axonframework/samples/trader/api/orders/transaction/commands.kt
+++ core-api/src/main/java/org/axonframework/samples/trader/api/orders/transaction/commands.kt
@@ -2,6 +2,7 @@ package org.axonframework.samples.trader.api.orders.transaction
 
 import org.axonframework.commandhandling.TargetAggregateIdentifier
 import org.axonframework.samples.trader.api.orders.OrderBookId
+import org.axonframework.samples.trader.api.orders.TransactionType
 import org.axonframework.samples.trader.api.portfolio.PortfolioId
 
 abstract class TransactionCommand(@TargetAggregateIdentifier open val transactionId: TransactionId)
@@ -11,24 +12,30 @@ abstract class AbstractStartTransactionCommand(
         open val orderBookId: OrderBookId,
         open val portfolioId: PortfolioId,
         open val tradeCount: Long,
-        open val itemPrice: Long
-) : TransactionCommand(transactionId)
+        open val pricePerItem: Long
+) : TransactionCommand(transactionId) {
+    abstract val transactionType: TransactionType
+}
 
 data class StartBuyTransactionCommand(
         override val transactionId: TransactionId,
         override val orderBookId: OrderBookId,
         override val portfolioId: PortfolioId,
         override val tradeCount: Long,
-        override val itemPrice: Long
-) : AbstractStartTransactionCommand(transactionId, orderBookId, portfolioId, tradeCount, itemPrice)
+        override val pricePerItem: Long
+) : AbstractStartTransactionCommand(transactionId, orderBookId, portfolioId, tradeCount, pricePerItem) {
+    override val transactionType: TransactionType = TransactionType.BUY
+}
 
 data class StartSellTransactionCommand(
         override val transactionId: TransactionId,
         override val orderBookId: OrderBookId,
         override val portfolioId: PortfolioId,
         override val tradeCount: Long,
-        override val itemPrice: Long
-) : AbstractStartTransactionCommand(transactionId, orderBookId, portfolioId, tradeCount, itemPrice)
+        override val pricePerItem: Long
+) : AbstractStartTransactionCommand(transactionId, orderBookId, portfolioId, tradeCount, pricePerItem) {
+    override val transactionType: TransactionType = TransactionType.SELL
+}
 
 data class CancelTransactionCommand(override val transactionId: TransactionId) : TransactionCommand(transactionId)
 
