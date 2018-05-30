1ff354592af9e9ed6be512dad7b06c0400d398b8
Jakub Pilimon <jakub.pilimon@gmail.com>
2018-05-30 11:47:54 +0200

    some web.xml content moved to be served with jar

--- web-ui/src/main/java/org/axonframework/samples/trader/webui/Application.java
+++ web-ui/src/main/java/org/axonframework/samples/trader/webui/Application.java
@@ -1,7 +1,22 @@
 package org.axonframework.samples.trader.webui;
 
+import com.opensymphony.module.sitemesh.filter.PageFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.Filter;
+import javax.validation.Validator;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +25,55 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    @Bean
+    Filter sitemash() {
+        return new PageFilter();
+    }
+
+
+}
+
+@Configuration
+class SecurityWebApplicationInitializer
+        extends AbstractSecurityWebApplicationInitializer {
+}
+
+@Configuration
+@ComponentScan(basePackages = { "org.axonframework.samples.trader.webui" })
+class ApplicationConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
+        registry.addResourceHandler("/style/**").addResourceLocations("/style/");
+
+    }
+
+    @Bean
+    MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
+        ms.setBasenames("/WEB-INF/messages/validation", "/WEB-INF/messages/fields");
+        return ms;
+    }
+
+    @Bean
+    Validator validator(MessageSource source) {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource());
+        return localValidatorFactoryBean;
+    }
 
 }
